33c4ea1dc06c0520a35694d2ffbff986e8f78bb3
SatoTaisei <t.sei43rm@gmail.com>
2024-08-14 17:08:25 +0900

    💄 フォントサイズを修正

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header className="w-full h-16 grid items-center p-4 bg-slate-100">
+    <header className="w-full h-16 text-xl font-bold grid items-center p-4 bg-slate-100">
       2 You Wine
     </header>
   );
