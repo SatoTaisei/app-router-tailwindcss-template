952f9f44a6e075f1f9867889bc61c717377a8c1a
SatoTaisei <t.sei43rm@gmail.com>
2024-08-14 17:10:08 +0900

    💄

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="w-full h-16 text-xl font-bold grid items-center p-4 bg-slate-100">
-      2 You Wine
+      Header
     </header>
   );
 };
